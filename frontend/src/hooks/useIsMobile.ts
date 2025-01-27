3a7e60d723cce038813ae039ce11ed7da1a893d1
Russellg <dudurussell@gmail.com>
2025-01-27 15:48:30 +0200

    feat: add custom hook to determine mobile view

---
+++ frontend/src/hooks/useIsMobile.ts
@@ -0,0 +1,17 @@
+import { twConfig } from "@/lib/utils";
+import { useEffect, useState } from "react";
+
+export default function useIsMobile() {
+  const MOBILE_WIDTH = Number(twConfig.theme.screens.sm.replace("px", ""));
+  const [isMobile, setIsMobile] = useState(window.innerWidth < MOBILE_WIDTH);
+
+  useEffect(() => {
+    const resizeFunc = () => setIsMobile(window.innerWidth < MOBILE_WIDTH);
+    window.addEventListener("resize", resizeFunc);
+    return () => window.removeEventListener("resize", resizeFunc);
+  }, [MOBILE_WIDTH]);
+
+  const viewPortWidth = window.innerWidth;
+  console.log({ MOBILE_WIDTH, viewPortWidth, isMobile });
+  return isMobile;
+}
