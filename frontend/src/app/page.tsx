9bab1bead757c992265ee5b3b3242ff72b1a9519
Russellg <dudurussell@gmail.com>
2025-01-27 15:50:46 +0200

    feat: lay out homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-1 flex-col items-center justify-center gap-16 p-8">
-      <main className="flex flex-grow flex-col items-center justify-center gap-5">
-        <Image
-          src="/logo.svg"
-          alt="Zim Auto Trade logo"
-          width={480}
-          height={38}
-          priority
-        />
-        <p className="text-gray-700">Coming Soon</p>
-      </main>
-
-      <footer>
-        <p className="text-gray-700">&copy; All Rights Reserved</p>
-      </footer>
-    </div>
+    <>
+      <>hero</>
+      <>popular preview</>
+      <>features (whats ZAT)</>
+      <>features (how it works)</>
+      <>cta (how to join)</>
+      <>cta (browse)</>
+      <>about (read more about platform)</>
+    </>
   );
 }
