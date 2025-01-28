dd315af21235419481c276a4f423455b7d52b964
Russellg <dudurussell@gmail.com>
2025-01-28 10:19:38 +0200

    feat: change logo

--- frontend/src/components/layout/Logo.tsx
+++ frontend/src/components/layout/Logo.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <Image
-      src="/logo.svg"
+      src="/logos/4.svg"
       alt="zim auto trade logo"
       width={200}
       height={100}
-      className="w-2rem order-2 w-[150px] sm:order-1 md:w-[200px]"
+      className="order-2 w-[150px] sm:order-1"
     />
   );
 }
