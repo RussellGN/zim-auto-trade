0e382774019530941c6adfab793ebf2165bcc900
Russellg <dudurussell@gmail.com>
2025-02-05 16:00:27 +0200

    feat: enhance NotFound component with Back button and link to homepage

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,13 +1,22 @@
+import BackBtn from "@/components/layout/BackBtn";
 import { Button } from "@/components/ui/button";
 import { Home } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 export default function NotFound() {
   return (
-    <div className="flex h-[80vh] flex-col items-center justify-center gap-3">
+    <div className="relative flex h-[80vh] flex-col items-center justify-center gap-3">
+      <div className="absolute left-0 top-0 m-5">
+        <BackBtn />
+      </div>
+
       <h2>This page does not exist</h2>
-      <Button className="bg-primary-default text-white">
-        Homepage <Home size={18} />
+
+      <Button asChild className="bg-primary-default text-white">
+        <Link href="/">
+          Homepage <Home size={18} />
+        </Link>
       </Button>
     </div>
   );
