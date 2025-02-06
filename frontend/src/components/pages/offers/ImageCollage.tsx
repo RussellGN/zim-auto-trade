2a74c2f12c3f6fc7393d7f89a4f4e752c12e5c88
Russellg <dudurussell@gmail.com>
2025-02-06 12:43:08 +0200

    feat: reload preview image when images change; add images container width prop

--- frontend/src/components/pages/offers/ImageCollage.tsx
+++ frontend/src/components/pages/offers/ImageCollage.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CustomImageProps } from "@/lib/types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Images } from "lucide-react";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
@@ -11,6 +11,7 @@ type propTypes = {
   alt: string;
   compact?: boolean;
   imgsClassname?: string;
+  imgsContainerMaxWidth?: string;
 };
 
 export default function ImageCollage({
@@ -18,8 +19,13 @@ export default function ImageCollage({
   alt,
   compact,
   imgsClassname,
+  imgsContainerMaxWidth = "100%",
 }: propTypes) {
   const [imageOnView, setImageOnView] = useState({ img: images[0], pos: 0 });
+
+  useEffect(() => {
+    setImageOnView({ img: images[0], pos: 0 });
+  }, [images]);
 
   return (
     <div>
@@ -38,7 +44,9 @@ export default function ImageCollage({
       </div>
 
       {!compact && (
-        <div className="flex items-center gap-2 overflow-x-auto p-2">
+        <div
+          className={`mx-auto flex max-w-[${imgsContainerMaxWidth}] items-center gap-1 overflow-x-auto p-1`}
+        >
           {images.map((img, index) => (
             <Image
               onClick={() => setImageOnView({ img, pos: index })}
@@ -48,7 +56,7 @@ export default function ImageCollage({
               width={800}
               height={450}
               className={cn(
-                `aspect-video h-[7rem] cursor-pointer rounded-lg border-2 bg-gray-700 object-cover hover:border-primary-light ${imageOnView.img.src === img.src ? "border-primary-light" : "border-transparent"}`,
+                `aspect-video h-[7rem] w-auto cursor-pointer rounded-lg border-2 bg-gray-700 object-cover hover:border-primary-light ${imageOnView.img.src === img.src ? "border-primary-light" : "border-transparent"}`,
                 imgsClassname,
               )}
             />
