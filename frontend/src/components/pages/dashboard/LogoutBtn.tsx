ddd1c4e8fc0c730e596d2a103ffec6ed6e2f1715
Russellg <dudurussell@gmail.com>
2025-02-10 16:49:00 +0200

    fix: handle logout errors

--- frontend/src/components/pages/dashboard/LogoutBtn.tsx
+++ frontend/src/components/pages/dashboard/LogoutBtn.tsx
@@ -3,15 +3,24 @@
 import { logoutAction } from "@/actions";
 import CustomModal from "@/components/general/CustomModal";
 import { Button } from "@/components/ui/button";
+import { toast } from "@/hooks/general/use-toast";
 import { LogOut } from "lucide-react";
 
 export default function LogoutBtn() {
+  function logout() {
+    logoutAction().catch(() =>
+      toast({
+        title: "Error logging out",
+        className: "bg-white shadow-lg",
+      })
+    );
+  }
   return (
     <CustomModal
       title="Logout"
       message="Are you sure you want to logout"
       icon={<LogOut size={18} />}
-      onProceed={logoutAction}
+      onProceed={logout}
       onCancel={() => {}}
       proceedText="Logout"
       cancelClassName="bg-primary-default text-white"
