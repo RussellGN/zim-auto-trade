25cd64b5b884da131e229dfe6e21dd2ebec394d9
Russellg <dudurussell@gmail.com>
2025-02-12 14:00:01 +0200

    feat: revamp footer

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import ScrollToTop from "./ScrollToTop";
 
 export default function Footer() {
   return (
-    <footer className="bg-primary-dark text-white">
+    <footer className="bg-black text-white">
       <div className="mx-auto flex max-w-screen-xl items-center justify-between p-5">
         <p className="">&copy; All Rights Reserved</p>
 
--- frontend/src/components/layout/ScrollToTop.tsx
+++ frontend/src/components/layout/ScrollToTop.tsx
@@ -6,9 +6,10 @@ import { ArrowUp } from "lucide-react";
 export default function ScrollToTop() {
   return (
     <Button
+      variant="outline"
       size="icon"
       title="back to top"
-      className="rounded-full bg-primary-default text-white shadow hover:bg-primary-dark"
+      className="rounded-full border-black bg-secondary-default text-black shadow hover:bg-secondary-dark"
       onClick={() => {
         if (typeof window !== "undefined") {
           window.scrollTo({ left: 0, top: 0 });
